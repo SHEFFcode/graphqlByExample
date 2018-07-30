a406366fa49bb3a09fe43ffa3bcf6659f59c68f4
SHEFFmachine <sheff@sheffmachine.com>
2018-07-29 19:13:30 -0700

    do some basic graphql stuff

--- hello-world-client/src/App.js
+++ hello-world-client/src/App.js
@@ -1,21 +1,38 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 class App extends Component {
+  state = {
+    greeting: ''
+  }
+  componentWillMount = () => {
+    fetch('http://localhost:9000/graphql', {
+      method: 'POST',
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify({ query: '{ greeting }' })
+    })
+      .then(response => response.json())
+      .then(responseBody =>
+        this.setState({ greeting: responseBody.data.greeting })
+      )
+  }
+
   render() {
+    const { greeting } = this.state
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">{greeting}</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
